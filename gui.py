04985f4844c4feabd1a80d6abc67e1833bd859b7
Gabe Freedman <gfreedman2108@comcast.net>
2024-12-22 12:46:25 -0600

    black

--- gui.py
+++ gui.py
@@ -12,12 +12,12 @@ from image_tools import resize_all
 
 
 class EntryWithPlaceholder(tk.Entry):
-    def __init__(self, master=None, placeholder="PLACEHOLDER", color='grey'):
+    def __init__(self, master=None, placeholder="PLACEHOLDER", color="grey"):
         super().__init__(master)
 
         self.placeholder = placeholder
         self.placeholder_color = color
-        self.default_fg_color = self['fg']
+        self.default_fg_color = self["fg"]
 
         self.bind("<FocusIn>", self.foc_in)
         self.bind("<FocusOut>", self.foc_out)
@@ -26,12 +26,12 @@ class EntryWithPlaceholder(tk.Entry):
 
     def put_placeholder(self):
         self.insert(0, self.placeholder)
-        self['fg'] = self.placeholder_color
+        self["fg"] = self.placeholder_color
 
     def foc_in(self, *args):
-        if self['fg'] == self.placeholder_color:
-            self.delete('0', 'end')
-            self['fg'] = self.default_fg_color
+        if self["fg"] == self.placeholder_color:
+            self.delete("0", "end")
+            self["fg"] = self.default_fg_color
 
     def foc_out(self, *args):
         if not self.get():
@@ -42,71 +42,81 @@ class ImageApp:
 
     def __init__(self):
         self.root = tk.Tk()
-        self.root.title('Image Resizing Application')
-        
-        self.folderLabel = tk.Label(self.root, text='Choose folder with images to resize:')
-        self.folderLabel.grid(column=0, row=0, sticky='EW', padx=4, pady=4)
+        self.root.title("Image Resizing Application")
+
+        self.folderLabel = tk.Label(
+            self.root, text="Choose folder with images to resize:"
+        )
+        self.folderLabel.grid(column=0, row=0, sticky="EW", padx=4, pady=4)
         self.folderVar = tk.StringVar(None)
         self.folderName = tk.Entry(self.root, textvariable=self.folderVar)
-        self.folderName.grid(column=1, row=0, sticky='EW', padx=4, pady=4)
+        self.folderName.grid(column=1, row=0, sticky="EW", padx=4, pady=4)
         self.folderName.update()
         self.folderName.focus_set()
-        
-        self.heightLabel = tk.Label(self.root, text='Enter desired image height in inches:')
-        self.heightLabel.grid(column=0,row=1,sticky='EW', padx=4, pady=4)
+
+        self.heightLabel = tk.Label(
+            self.root, text="Enter desired image height in inches:"
+        )
+        self.heightLabel.grid(column=0, row=1, sticky="EW", padx=4, pady=4)
         self.heightVar = tk.StringVar(None)
         self.heightEntry = tk.Entry(self.root, bd=1, textvariable=self.heightVar)
-        self.heightEntry.grid(column=1, row=1, sticky='EW', padx=4, pady=4)
-    
-        self.button = tk.Button(self.root, text='Browse', command=self.browse)
-        self.button.grid(column=2, row=0, sticky='EW', padx=4, pady=4)
-        
-        self.identLabel = tk.Label(self.root, text='Identifier to add to new folder name:')
-        self.identLabel.grid(column=0, row=2, sticky='EW', padx=4, pady=4)
-        self.identEntry = EntryWithPlaceholder(self.root, '(Optional)')
-        self.identEntry.grid(column=1, row=2, sticky='EW', padx=4, pady=4)
-        
+        self.heightEntry.grid(column=1, row=1, sticky="EW", padx=4, pady=4)
+
+        self.button = tk.Button(self.root, text="Browse", command=self.browse)
+        self.button.grid(column=2, row=0, sticky="EW", padx=4, pady=4)
+
+        self.identLabel = tk.Label(
+            self.root, text="Identifier to add to new folder name:"
+        )
+        self.identLabel.grid(column=0, row=2, sticky="EW", padx=4, pady=4)
+        self.identEntry = EntryWithPlaceholder(self.root, "(Optional)")
+        self.identEntry.grid(column=1, row=2, sticky="EW", padx=4, pady=4)
+
         self.borderVar = tk.IntVar(None)
-        self.borderCheck = tk.Checkbutton(self.root, text='Add border? (1px)', variable=self.borderVar)
-        self.borderCheck.grid(column=1, row=3, sticky='EW', padx=4, pady=4)        
-        
-        self.bigButton = tk.Button(self.root, text='Resize', command=self.gather_resize_params)
-        self.bigButton.grid(column=1, row=4, sticky='EW', padx=4, pady=4)
-        
+        self.borderCheck = tk.Checkbutton(
+            self.root, text="Add border? (1px)", variable=self.borderVar
+        )
+        self.borderCheck.grid(column=1, row=3, sticky="EW", padx=4, pady=4)
+
+        self.bigButton = tk.Button(
+            self.root, text="Resize", command=self.gather_resize_params
+        )
+        self.bigButton.grid(column=1, row=4, sticky="EW", padx=4, pady=4)
+
         self.root.mainloop()
-    
+
     def browse(self):
-        self.filename = filedialog.askdirectory(parent=self.root,title='Choose a file')
+        self.filename = filedialog.askdirectory(parent=self.root, title="Choose a file")
         self.folderVar.set(self.filename)
-    
+
     def clear_box(self):
         self.identVar.delete(0, tk.END)
         return
 
     def handle_event(self, event):
-        print('Handling an event')
-    
+        print("Handling an event")
+
     def gather_resize_params(self):
         folder = self.folderVar.get()
-        if folder == '':
-            messagebox.showerror('error', 'try again')
+        if folder == "":
+            messagebox.showerror("error", "try again")
             return
-        
+
         height = self.heightEntry.get()
         try:
             height = float(height)
         except ValueError:
-            messagebox.showerror('error', 'try again')
+            messagebox.showerror("error", "try again")
             return
-        
+
         ident = self.identEntry.get()
-        if ident in ['', '(Optional)']:
-            ident = str(height) + 'inch'
-        
+        if ident in ["", "(Optional)"]:
+            ident = str(height) + "inch"
+
         border = self.borderVar.get()
-        
+
         resize_all(folder, height, ident, border)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ImageApp()
--- image_tools.py
+++ image_tools.py
@@ -13,45 +13,44 @@ from PIL import Image, ExifTags, ImageOps
 
 
 def images_in_dir(folder):
-    """ Takes in name of directory to search for images. Returns dictionary
-        with key : value pairs of relative_dir : absolute_dir pointing
-        to image files.
+    """Takes in name of directory to search for images. Returns dictionary
+    with key : value pairs of relative_dir : absolute_dir pointing
+    to image files.
     """
-    valid_extensions = ('.jpg', '.jpeg', '.JPG', '.JPEG')
-    
+    valid_extensions = (".jpg", ".jpeg", ".JPG", ".JPEG")
+
     rel_dir = [file for file in os.listdir(folder) if file.endswith(valid_extensions)]
-    
-    abs_dir = [folder + '\\' + image for image in rel_dir]
-    
+
+    abs_dir = [folder + "\\" + image for image in rel_dir]
+
     imd = list(zip(rel_dir, abs_dir))
-    
+
     return imd
 
 
 def make_parallel_dir(folder, ident):
-    """ Takes in name of directory containing images. Moves up to parent
-        directory and creates new folder named after original directory plus
-        an identifying string. Returns new folder path
+    """Takes in name of directory containing images. Moves up to parent
+    directory and creates new folder named after original directory plus
+    an identifying string. Returns new folder path
     """
-    
+
     parent_dir = os.path.normpath(folder + os.sep + os.pardir)
-    
+
     folder_basename = os.path.relpath(folder, parent_dir)
-    
-    new_folder_name = folder_basename + '_' + ident
-    
+
+    new_folder_name = folder_basename + "_" + ident
+
     folder_path = os.path.join(parent_dir, new_folder_name)
     if os.path.isdir(folder_path):
         shutil.rmtree(folder_path)
-    
+
     os.mkdir(folder_path)
     return folder_path
 
 
 def get_monitor_dpi():
-    """ Retrieves DPI value for given monitor
-    """
-    
+    """Retrieves DPI value for given monitor"""
+
     LOGPIXELSX = 88
     LOGPIXELSY = 90
     dc = windll.user32.GetDC(0)
@@ -62,41 +61,40 @@ def get_monitor_dpi():
 
 
 def resize_image(image_tup, new_height, resize_folder, border):
-    """ Resizes image I hope
-    """
+    """Resizes image I hope"""
     i = Image.open(image_tup[1])
-    
+
     try:
-        for orientation in ExifTags.TAGS.keys(): 
-            if ExifTags.TAGS[orientation] == 'Orientation':
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation] == "Orientation":
                 break
         exif = dict(i._getexif().items())
-    
-        if exif[orientation] == 3: 
+
+        if exif[orientation] == 3:
             i = i.rotate(180, expand=True)
-        elif exif[orientation] == 6: 
+        elif exif[orientation] == 6:
             i = i.rotate(270, expand=True)
-        elif exif[orientation] == 8: 
+        elif exif[orientation] == 8:
             i = i.rotate(90, expand=True)
     except (AttributeError, KeyError, IndexError):
         pass
-    
+
     dpi = get_monitor_dpi()
     orig_size_px = i.size
-    
+
     aspect_ratio = orig_size_px[0] / orig_size_px[1]
-    
+
     new_height_px = new_height * dpi[0]
     new_width_px = new_height_px * aspect_ratio
     new_dims = (int(new_width_px), int(new_height_px))
 
     new_im = i.resize(new_dims, PIL.Image.ANTIALIAS)
-    
+
     if border == 1:
         border_width_pt = 1
         border_width_px = int(border_width_pt * (dpi[0] / 72))
         new_im = ImageOps.expand(new_im, border=border_width_px)
-    
+
     save_path = os.path.join(resize_folder, image_tup[0])
     new_im.save(save_path)
     i.close()
